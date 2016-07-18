895260ffbc8579108774c67622020267d52e7faf
Dmitry Pisarev <d1mich@voodoo-mobile.com>
2016-07-18 13:38:51 +0600

    [!] Added config for sleep time

--- src/module/Module.php
+++ src/module/Module.php
@@ -9,14 +9,20 @@ class Module extends \yii\base\Module implements BootstrapInterface
 
     public $workers = [];
 
+    public $watcherSleep = 60; //seconds
+
+    public $workerSleep = 10; //seconds
+
     public function init()
     {
         $this->controllerMap['watcher'] = [
             'class' => 'vm\background\module\commands\WatcherController',
+            'sleep' => $this->watcherSleep,
         ];
 
         $this->controllerMap['worker'] = [
             'class' => 'vm\background\module\commands\WorkerController',
+            'sleep' => $this->workerSleep,
         ];
     }
 
